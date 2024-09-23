b5f596ea6403254348bb84a629a38604a252c351
vireshpati <viresh@patifamily.com>
2024-09-22 21:13:24 -0400

    refactor

--- app/main.py
+++ app/main.py
@@ -9,5 +9,5 @@ app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(staff.router, prefix="/staff", tags=["staff"])
 
 # Serve static files (for frontend)
-app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
+app.mount("/", StaticFiles(directory="app/frontend", html=True), name="frontend")
 
