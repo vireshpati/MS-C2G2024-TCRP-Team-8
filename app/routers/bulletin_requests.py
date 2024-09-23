022407cec13c43de48339d8e3c736612309c9748
tcheung0 <cheungtyt@gmail.com>
2024-09-23 01:23:11 -0700

    finish with basic functionalites

--- app/routers/bulletin_requests.py
+++ app/routers/bulletin_requests.py
@@ -11,39 +11,28 @@ router = APIRouter()
 firestore_service = FirestoreService()
 
 @router.post("/bulletin", response_model=Request)
-def create_request(
-    uid: str = Depends(get_current_user),
-    request: RequestCreate):
+def create_request(request: Request):
     try:
-        logging.info(f"Creating request for uid: {uid}")
-        request.author_uid = uid
-        bulletin_service.create_request_post(uid, request.dict())
+        logging.info("Creating request")
+        request.created = datetime.utcnow()
+        bulletin_service.create_request_post(request.dict())
         return request
     except Exception as e:
         logging.error(f"Error in create_request: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-@router.post("/bulletin/{post_uid}", response_model=Comment)
-def create_comment(
-    uid: str = Depends(get_current_user),
-    comment: CommentCreate):
+@router.post("/bulletin/{post_uid}/comments", response_model=Comment)
+def create_comment(comment: Comment):
     try:
         logging.info(f"Creating comment for post_uid: {comment.post_uid}")
-        comment.author_uid = uid
-        bulletin_service.create_comment_reply(uid, comment.post_uid)
-
-        doc_ref = db.collection('comments').add(comment.dict())
-        doc_ref = db.collection('requests').document(comment.post_uid)
-        doc_ref.update({
-            'number_of_comments': firestore.Increment(1)
-        })
+        comment.created = datetime.utcnow()
+        bulletin_service.create_comment_reply(comment.post_uid)
         logging.info("Comment created successfully.")
-        return request
+        return comment
     except Exception as e:
         logging.error(f"Error in create_comment: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-# done
 @router.get("/bulletin/{post_uid}", response_model=Request)
 async def get_request(post_uid: int):
     try:
@@ -60,57 +49,39 @@ async def get_request(post_uid: int):
 @router.get("/bulletin/{post_uid}/comments", response_model=Request)
 async def get_comments(post_uid: int):
     try:
-        logging.info(f"Fetching request with post_uid: {post_uid}")
-        request_data = bulletin_service.get_request_post(post_uid)
-        if request_data:
-            return Request(**request_data)
+        logging.info(f"Fetching comments for request with post_uid: {post_uid}")
+        comment_data = bulletin_service.get_comment_replies(post_uid)
+        if comment_data:
+            return [Comment(**data) for data in comment_data]
         else:
             raise HTTPException(status_code=404, detail="Request post not found.")
     except Exception as e:
-        logging.error(f"Error in get_request: {e}")
+        logging.error(f"Error in get_comments: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
-
 
 @router.get("/bulletin", response_model=Request)
-async def get_relavent_request_list(
-    sort_field: str = Query(None, description="Sort by 'post_creation_date' or 'confirm_by'"),
-    asc: bool = Query(False, description="True for ascending order, False (default) for descending order"),
-    filter_tag: str = Query(None, description="one of 'babysitting', 'tutoring', 'pickupchild', 'mealshare', 'needride', 'others'"),
-    page: int = 1, 
-    limit: int = 20,
-    resolved_requests: bool = Query(False, description="True shows resolved requests, False (default) shows unresolved requestsr")
-):
+async def get_relavent_request_list(page: int = 1, limit: int = 30):
     try:
-        logging.info(f"Fetching bulletin board")
-        request_data = bulletin_service.get_relavent_request_list()
+        logging.info("Fetching bulletin board")
+        request_data = bulletin_service.get_relavent_request_list(page, limit)
         if request_data:
-            return Request(**request_data)
+            return [Request(**data) for data in request_data]
         else:
-            raise HTTPException(status_code=404, detail="Request post not found.")
+            raise HTTPException(status_code=404, detail="Request posts not found.")
     except Exception as e:
-        logging.error(f"Error in get_request: {e}")
+        logging.error(f"Error in get_relavent_request_list: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
     
-@router.put("/bulletin/{post_uid}/edit", response_model=schemas.Request)
-def update_request(post_uid: str,request: Request):
+@router.put("/bulletin/{post_uid}", response_model=schemas.Request)
+def update_request(request: Request):
     try:
-        logging.info(f"Updating request with post_uid: {post_uid}")
-        doc_ref = db.collection('requests').document(post_uid)
-        doc = doc_ref.get()
-        if not doc:
-            raise HTTPException(status_code=404, detail="Request not found")
-        doc_data = doc.to_dict()
-        if doc_data.get('author_uid') != uid:
-            raise HTTPException(status_code=403, detail="User not authorized to edit this request")
-        request.created = datetime.utcnow()
-        doc_ref.update(request.dict())
-        logging.info("Request updated successfully.")
+        logging.info(f"Updating request with post_uid: {request.post_uid}")
+        bulletin_service.update_request_post(request.dict())
         return request
     except Exception as e:
-        logging.error(f"Error in update_request: {e}")
+        logging.error(f"Error in update_profile: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-# done
 @router.put("/bulletin/{post_uid}/resolve", response_model=schemas.Request)
 def resolve_request(post_uid: str):
     try:
@@ -125,7 +96,6 @@ def resolve_request(post_uid: str):
         logging.error(f"Error toggling active status for UID: {request.target_uid}: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-# done
 @router.delete("/bulletin/{post_id}")
 def delete_request(post_uid: int):
     try:
--- app/services/bulletin_service.py
+++ app/services/bulletin_service.py
@@ -7,41 +7,37 @@ class BulletinService:
     def __init__(self):
         self.db = firestore.client()
 
-    # done
-    def create_request_post(self, uid, request_data):
+    def create_request_post(self, request_data):
         try:
             doc_ref = self.db.collection('requests').add(request_data)
             logging.info("Request created successfully.")
         except Exception as e:
             logging.error(f"Error in create_request: {e}")
-
-    # done
+=
     def create_comment_reply(self, uid, comment_data):
         try:
-            doc_ref = self.db.collection('comments').add(comment_data)
             doc_ref = self.db.collection('requests').document(comment_data['post_uid'])
             doc_ref.update({
                 'number_of_comments': firestore.Increment(1)
             })
+            doc_ref = self.db.collection('comments').add(comment_data)
             logging.info("Comment created successfully.")
         except Exception as e:
             logging.error(f"Error in create_commentt: {e}")
 
-    # done
     def get_request_post(self, post_uid):
         try:
-            doc_ref = self.db.collection('request').document(post_uid)
+            doc_ref = self.db.collection('requests').document(post_uid)
             doc = doc_ref.get()
             if doc.exists:
                 return doc.to_dict()
             else:
-                logging.warning(f"Request not found for uid: {uid}")
+                logging.warning(f"Request not found for post_uid: {post_uid}")
                 return None
         except Exception as e:
             logging.error(f"Error retrieving request: {e}")
             return None
 
-    # done
     def get_comment_replies(self, post_uid):
         try:
             docs_ref = self.db.collection('comments')
@@ -53,32 +49,42 @@ class BulletinService:
             return all_comments
         except Exception as e:
             logging.error(f"Error retrieving all comments: {e}")
-            raise
 
-    # done
-    def update_request_post(self, post_uid, request_data):
+    def get_relavent_requests(self, page, limit):
         try:
-            doc_ref = self.db.collection('requests').document(post_uid)
+            docs_ref = self.db.collection('requests')
+            now = datetime.datetime.utcnow()
+            docs = docs_ref.where('deleted', '==', False).where('resolved', '==', resolved_requests) \
+                            .where('date_help_needed', '>=', now).limit(limit).offset(offset).stream()
+            all_requests = []
+            for doc in docs:
+                request_data = doc.to_dict()
+                all_requests.append(request_data)
+            return all_requests
+        except Exception as e:
+            logging.error(f"Error retrieving relavent requests: {e}")
+
+    def update_request_post(self, request_data):
+        try:
+            doc_ref = self.db.collection('requests').document(request_data['post_uid'])
             doc_ref.set(request_data, merge=True)
             logging.info("Request updated successfully.")
         except Exception as e:
             logging.error(f"Error updating request: {e}")
 
-    # done
     def toggle_request_resolved_status(self, post_uid):
         try:
             request_data = self.get_request_post(post_uid)
             if not request_data:
-                logging.warning(f"Request not found for uid: {post_uid}")
-                raise ValueError(f"Request not found for uid: {post_uid}")
+                logging.warning(f"Request not found for post_uid: {post_uid}")
+                raise ValueError(f"Request not found for post_uid: {post_uid}")
             request_data['resolved'] = not request_data['resolved']
-            self.update_request_post(uid, post_uid, request_data)
+            self.update_request_post(request_data)
             return request_data
         except Exception as e:
             logging.error(f"Error toggling request resolved status request: {e}")
             raise
 
-    #done
     def delete_request_post(self, post_uid):
         try:
             doc_ref = self.db.collection('requests').document(post_uid)
@@ -86,21 +92,5 @@ class BulletinService:
             logging.info("Request deleted successfully.")
         except Exception as e:
             logging.error(f"Error deleting request: {e}")
-
-    # done
-    def get_relavent_requests(self):
-        try:
-            docs_ref = self.db.collection('requests')
-            now = datetime.datetime.utcnow()
-            docs = docs_ref.where('deleted', '==', False).where('resolved', '==', resolved_requests) \
-                            .where('date_help_needed', '>=', now).stream()
-            all_requests = []
-            for doc in docs:
-                request_data = doc.to_dict()
-                all_requests.append(request_data)
-            return all_requests
-        except Exception as e:
-                logging.error(f"Error retrieving relavent requests: {e}")
-                raise
 
 
